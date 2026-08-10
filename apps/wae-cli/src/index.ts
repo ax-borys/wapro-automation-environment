41bc55253c067575d3b99a65e43a492b15fef55e
Alex Borysiuk <alexborysiuk@pm.me>
2026-08-10 23:35:52 +0200

    feat: add possibility to read receipts data from json file

--- apps/wae-cli/src/index.ts
+++ apps/wae-cli/src/index.ts
@@ -1,37 +1,28 @@
+import fs from 'fs';
 import { db } from '@wae/db';
 import { createReceipt } from '@wae/wapro-mag-create-receipt';
 
-await db.transaction(async (tx) => {
-   const params: Parameters<typeof createReceipt>[1] = {
-      companyId: 1,
-      cashRegisterId: 1,
-      counterPartyId: 1,
-      userId: 3000001,
-      deposit: 0,
-      paymentDeadline: new Date(),
-      paymentFormat: 'przelew',
-      pricingType: 'Brutto',
-      stockId: 1,
-      positions: [
-         {
-            productId: 412,
-            quantity: 2,
-            priceNetto: 244,
-            priceBrutto: 300.12,
-            vatCode: '23',
-            discount: 0,
-         },
-         {
-            productId: 412,
-            quantity: 1,
-            priceNetto: 300,
-            priceBrutto: 369,
-            vatCode: '23',
-            discount: 0,
-         },
-      ],
-   };
+const receiptsData = JSON.parse(fs.readFileSync(process.argv[2], 'utf-8'));
+console.log(receiptsData);
 
-   const result = await createReceipt(tx, params);
-   console.log(result);
+await db.transaction(async (tx) => {
+   const receiptsParams: Parameters<typeof createReceipt>[1][] =
+      receiptsData.map(
+         (
+            d: Omit<Parameters<typeof createReceipt>[1], 'paymentDeadline'> & {
+               paymentDeadline: string;
+            },
+         ) => ({
+            ...d,
+            paymentDeadline: new Date(d.paymentDeadline),
+         }),
+      );
+
+   const results = [];
+
+   for (const p of receiptsParams) {
+      results.push(await createReceipt(tx, p));
+   }
+
+   console.log('Operation succeeded./n', results);
 });
