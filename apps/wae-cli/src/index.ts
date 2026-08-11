a9afdfcd192b349a9d2c70567965a39b30ffa597
AzZiMyP <azzimyp@gmail.com>
2026-08-11 20:42:27 +0200

    refactor: add possibility to create and use many commands

--- apps/wae-cli/src/index.ts
+++ apps/wae-cli/src/index.ts
@@ -2,27 +2,40 @@ import fs from 'fs';
 import { db } from '@wae/db';
 import { createReceipt } from '@wae/wapro-mag-create-receipt';
 
-const receiptsData = JSON.parse(fs.readFileSync(process.argv[2], 'utf-8'));
-console.log(receiptsData);
+const commands: Record<string, (arg: string) => Promise<number | void>> = {
+   'create-receipts': createReceiptsCommand,
+};
 
-await db.transaction(async (tx) => {
-   const receiptsParams: Parameters<typeof createReceipt>[1][] =
-      receiptsData.map(
-         (
-            d: Omit<Parameters<typeof createReceipt>[1], 'paymentDeadline'> & {
-               paymentDeadline: string;
-            },
-         ) => ({
-            ...d,
-            paymentDeadline: new Date(d.paymentDeadline),
-         }),
-      );
+async function createReceiptsCommand(arg: string) {
+   const receiptsData = JSON.parse(fs.readFileSync(arg, 'utf-8'));
 
-   const results = [];
+   await db.transaction(async (tx) => {
+      const receiptsParams: Parameters<typeof createReceipt>[1][] =
+         receiptsData.map(
+            (
+               d: Omit<
+                  Parameters<typeof createReceipt>[1],
+                  'paymentDeadline'
+               > & {
+                  paymentDeadline: string;
+               },
+            ) => ({
+               ...d,
+               paymentDeadline: new Date(d.paymentDeadline),
+            }),
+         );
 
-   for (const p of receiptsParams) {
-      results.push(await createReceipt(tx, p));
-   }
+      const results = [];
 
-   console.log('Operation succeeded./n', results);
+      for (const p of receiptsParams) {
+         results.push(await createReceipt(tx, p));
+      }
+
+      console.log('Operation succeeded.');
+      console.log(results);
+   });
+}
+
+process.argv.forEach((arg: string, i, arr) => {
+   commands[arg]?.(arr[i + 1]);
 });
