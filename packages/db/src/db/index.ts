01cfebcc8b037389c3f8cdec050cefd30b15eb9c
AzZiMyP <azzimyp@gmail.com>
2026-08-11 21:33:41 +0200

    feat: create global wae-cli bin

--- packages/db/src/db/index.ts
+++ packages/db/src/db/index.ts
@@ -1,4 +1,20 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const envPath = path.resolve(__dirname, '../../../.env');
+
+dotenv.config({
+   path: envPath,
+});
+
 import { drizzle } from 'drizzle-orm/node-mssql';
+
+if (!process.env.DATABASE_URL) {
+   throw new Error('DATABASE_URL is not set');
+}
 
 export const db = drizzle(process.env.DATABASE_URL!);
