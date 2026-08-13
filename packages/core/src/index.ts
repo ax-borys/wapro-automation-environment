fe56d0d3432c7921823330acc5af09b5c558e8fc
Alex Borysiuk <alexborysiuk@pm.me>
2026-08-13 11:49:12 +0200

    feat: create price distributing function

---
+++ packages/core/src/index.ts
@@ -0,0 +1,13 @@
+export default function distributeNumber(total: number, parts: number): number[] {
+   const base = Math.floor(total / parts);
+
+   const remainder = total % parts;
+
+   const result = [0];
+
+   for (let i = 0; i < parts; i++) {
+      result[i] = (base + (i < remainder ? 1 : 0))
+   }
+
+   return result;
+}
