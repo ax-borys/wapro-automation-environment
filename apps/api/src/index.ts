1fb994e8c51146fe0935df63039a1f639e69c366
Alex Borysiuk <alexborysiuk@pm.me>
2026-08-15 20:05:44 +0200

    feat: enable cors and serve static files

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,31 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { recordReceiptHandler } from './receipt/controller.js';
 
+import { serveStatic } from '@hono/node-server/serve-static';
+import { cors } from 'hono/cors';
+
 const app = new Hono();
+
+app.use(
+   '*',
+   cors({
+      origin: 'http://localhost:8081',
+   }),
+);
+
+app.use(
+   '/public/*',
+   serveStatic({
+      root: path.resolve(__dirname, '../'),
+   }),
+);
 
 app.get('/', (c) => {
    return c.text('Hello Hono!');
