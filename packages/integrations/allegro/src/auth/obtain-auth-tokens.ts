010ffc0eb2dde01f5d0781eee768da29688eab61
Alex Borysiuk <alexborysiuk@pm.me>
2026-09-01 18:43:05 +0200

    feat: add token freshness checking before return it from store

--- packages/integrations/allegro/src/auth/obtain-auth-tokens.ts
+++ packages/integrations/allegro/src/auth/obtain-auth-tokens.ts
@@ -1,21 +1,41 @@
 import { fetchAuthTokens } from './fetch-auth-tokens';
 import { store } from '../store/store';
 import jwt, { JwtPayload } from 'jsonwebtoken';
+import * as v from 'valibot';
 
-interface AllegroJWTPayload extends JwtPayload {
+interface AllegroJwtPayload extends JwtPayload {
    iss: string;
    user_name: string;
-   scope: string;
+   scope: string[];
    allegro_api: boolean;
    exp: number;
    client_id: string;
    jti: string;
 }
 
-const validToken = (token: string) => {
+const allegroJwtPayloadSchema = v.object({
+   iss: v.string(),
+   user_name: v.string(),
+   scope: v.array(v.string()),
+   allegro_api: v.boolean(),
+   exp: v.pipe(v.number(), v.integer()),
+   client_id: v.string(),
+   jti: v.string(),
+}) satisfies v.GenericSchema<AllegroJwtPayload>;
+
+const checkTokenFreshness = (token: string): boolean => {
    const payload: JwtPayload | null = jwt.decode(token, { json: true });
 
-   if (!payloadString) return false;
+   if (!payload) return false;
+
+   try {
+      const validPayload = v.parse(allegroJwtPayloadSchema, payload);
+      const isFresh = validPayload.exp * 1000 <= Date.now();
+
+      return isFresh;
+   } catch (error) {
+      return false;
+   }
 };
 
 export async function obtainAuthTokens(): Promise<{
@@ -25,7 +45,7 @@ export async function obtainAuthTokens(): Promise<{
    const { refreshToken, accessToken, setRefreshToken, setAccessToken } =
       store.getState();
 
-   if (refreshToken && accessToken) {
+   if (refreshToken && accessToken && checkTokenFreshness(accessToken)) {
       return { accessToken, refreshToken };
    }
 
